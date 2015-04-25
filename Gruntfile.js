1ad744e7db439d6275875acffb97af66bf3719f2
Thomas Leen <tl@thomasleen.com>
2015-04-25 10:26:54 -0400

    added basic structure

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = function(grunt){
+
+
+  grunt.initConfig({
+    
+    jshint : {
+      all : {
+	files : {
+	  src : ['./package.json', './Gruntfile.js','./index.js','./test/index.js']
+	},
+	options : {
+	  expr : true,
+	  mocha : true,
+	  node : true
+	}
+      }
+    },
+
+    test: {
+      options : {
+	reporter : 'spec',
+        require : ['should'],
+        ui : 'bdd'
+      },
+      files : ['test/index.js']
+    }
+  }
+);
+
+  grunt.loadNpmTasks('grunt-cafe-mocha');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  grunt.renameTask('cafemocha', 'test');
+
+  grunt.registerTask('default', ['jshint', 'test']);
+
+
+};
---
+++ test/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+describe('most basic test', function(){
+  it('should always be true', function(){
+    (1 === 1).should.be.true;
+  });
+});
